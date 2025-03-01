85bf6ea8fee1919a241f25e46fe949370b698e2c
IrnandaNanda <irnandatri1509@gmail.com>
2025-03-02 00:36:33 +0700

    .

--- 2. Belajar Dasar Pemrogaman Javascript/Sang pengatur Program/Kuis Coding/main.js
+++ 2. Belajar Dasar Pemrogaman Javascript/Sang pengatur Program/Kuis Coding/main.js
@@ -30,7 +30,7 @@ function categorizeNumber(input) {
 
   function prima(num) {
     if (num < 2) return false;
-    for (let i = 2; i <= Math.sqrt(num); i++) {
+    for (i = 2; i <= Math.sqrt(num); i++) {
       if (num % i === 0) {
         return false;
       }
