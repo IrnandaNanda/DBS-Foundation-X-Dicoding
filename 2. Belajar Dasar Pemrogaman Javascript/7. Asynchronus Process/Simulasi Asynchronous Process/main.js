afc6129fe0adfafc02f798dc9a188f6ad1dcb193
IrnandaNanda <irnandatri1509@gmail.com>
2025-03-07 00:39:40 +0700

    Asynchronous

---
+++ 2. Belajar Dasar Pemrogaman Javascript/7. Asynchronus Process/Simulasi Asynchronous Process/main.js
@@ -0,0 +1,14 @@
+// const estimationTime = 3_000;
+
+// setTimeout(() => {
+//   console.log('Hello, world!'); // ? Hello world akan muncul setelah 3 detik karena kita menggunakan setTimeout
+// }, estimationTime);
+
+import { makeCoffee } from './coffe';
+
+console.log('Saya memesan kopi di kafe.');
+
+makeCoffee();
+
+console.log('Pramusaji memberikan kopi pesanan.');
+console.log('Saya mendapatkan kopi dan menghabiskannya.');
